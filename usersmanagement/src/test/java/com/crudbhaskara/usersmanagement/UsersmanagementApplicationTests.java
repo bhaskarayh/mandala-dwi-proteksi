e24bfe784c50da0dad5a757c50501d886ed3953f
bhaskarayh <bhaskarayh@gmail.com>
2024-06-27 23:10:21 +0700

    first commit

---
+++ usersmanagement/src/test/java/com/crudbhaskara/usersmanagement/UsersmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudbhaskara.usersmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
